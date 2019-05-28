d6e7695552d2e0b6cc863fae8580682effbf973d
Saeed Rahman <saeedrahman@cdnis.edu.hk>
2019-05-28 08:19:16 +0800

    Initial Commit

---
+++ Quizly/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quizly
+//
+//  Created by Saeed Rahman on 28/5/2019.
+//  Copyright © 2019 Saeed Rahman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
