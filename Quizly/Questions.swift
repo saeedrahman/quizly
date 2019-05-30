4d34fe0626d946972141cde7752a07de968fe082
Saeed Rahman <saeedrahman@cdnis.edu.hk>
2019-05-31 07:59:11 +0800

    Added a question

--- Quizly/Questions.swift
+++ Quizly/Questions.swift
@@ -15,6 +15,6 @@ class Questions {
     init() {
         questions.append(Question(questionText: "What is the capital city of the United Kingdom?", choiceOne: "London", choiceTwo: "Manchester", choiceThree: "Sheffield", choiceFour: "Bristol", correctAnswer: 1))
         questions.append(Question(questionText: "How many states are there in the United States?", choiceOne: "51", choiceTwo: "52", choiceThree: "35", choiceFour: "17", correctAnswer: 2))
-        questions.append(Question(questionText: "Who is the most popular YouTube celebrity?", choiceOne: "PewdiePie", choiceTwo: "T-Series", choiceThree: "Brian Voong", choiceFour: "Logan Paul", correctAnswer: 1))
+        questions.append(Question(questionText: "First China War was fought between which countries?", choiceOne: "China and Britain", choiceTwo: "China and France", choiceThree: "China and Egypt", choiceFour: "China and Greece", correctAnswer: 1))
     }
 }
