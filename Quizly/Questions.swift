569cbab81afebb05ed5c1733188753fa1d685217
Saeed Rahman <saeedrahman@cdnis.edu.hk>
2019-05-31 14:38:28 +0800

    Removed a question

--- Quizly/Questions.swift
+++ Quizly/Questions.swift
@@ -15,6 +15,6 @@ class Questions {
     init() {
         questions.append(Question(questionText: "What is the capital city of the United Kingdom?", choiceOne: "London", choiceTwo: "Manchester", choiceThree: "Sheffield", choiceFour: "Bristol", correctAnswer: 1))
         questions.append(Question(questionText: "How many states are there in the United States?", choiceOne: "51", choiceTwo: "52", choiceThree: "35", choiceFour: "17", correctAnswer: 2))
-        questions.append(Question(questionText: "First China War was fought between which countries?", choiceOne: "China and Britain", choiceTwo: "China and France", choiceThree: "China and Egypt", choiceFour: "China and Greece", correctAnswer: 1))
+        questions.append(Question(questionText: "Which of the following men does not have a chemical element named after him?", choiceOne: "Albert Einstein", choiceTwo: "Niels Bohr", choiceThree: "Isaac Newton", choiceFour: "Enrico Fermi", correctAnswer: 3))
     }
 }
